5c2e3bb1fa1a6ac1b6e8c8c3d13fd80ca1f6acd6
yves songolo <yves.songolo@gmail.com>
2018-01-28 11:56:48 -0800

    develop ios client

---
+++ voice-app-clinet/voice-app-clinet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voice-app-clinet
+//
+//  Created by Yveslym on 1/28/18.
+//  Copyright © 2018 Yveslym. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
