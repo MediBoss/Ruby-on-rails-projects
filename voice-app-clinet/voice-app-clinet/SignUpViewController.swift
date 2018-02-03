fc80ff97a361f5b4cdbbbc97f60fc973a71e3f64
yves songolo <yves.songolo@gmail.com>
2018-02-03 15:44:04 -0800

    new push

--- voice-app-clinet/voice-app-clinet/SignUpViewController.swift
+++ voice-app-clinet/voice-app-clinet/SignUpViewController.swift
@@ -12,13 +12,16 @@ class SignUpViewController: UIViewController {
     
     // - MARK: IBOUTLET
     @IBOutlet weak var firstNameTextField : UITextField!
+    @IBOutlet weak var lastNameTextField : UITextField!
+    
     @IBOutlet weak var emailAddressTextField : UITextField!
     @IBOutlet weak var userNameTextField : UITextField!
     @IBOutlet weak var passwordTextField : UITextField!
     @IBOutlet weak var confirmPasswordTextField : UITextField!
+    @IBOutlet weak var registerButton: UIButton!
     
     // - MARK: PROPERTIES
-    
+    let placeHolder = ["un":"Username", "pw":"Password", "em":"Email","cp":"Confirm Password","fn":"First Name","ln":"Last Name"]
     
     // - MARK: IBACTIONS
     @IBAction func registerButtonTyped(_ sender: Any){
@@ -29,33 +32,71 @@ class SignUpViewController: UIViewController {
     }
     
     
-    // - MARK: PROPERTIES
-    
-    
     // - MARK: METHODS
-    /*
-    func verifyPassword(password: String, confirmPassword: String) -> Bool{
+    
+    func verifyPassword() -> Bool{
         
-        if(password != confirmPassword){
-            
-            
+        guard let password = self.passwordTextField.text, let confirmPassword =     self.confirmPasswordTextField.text else {return false}
+        
+        if password == confirmPassword{
+            if !password.isEmpty && !confirmPassword.isEmpty{
+                return true
+            }
         }
-        
+        return false
     }
- */
     
     // - MARK - VIEW CONTROLLER LIFE CYCLE
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.registerButton.isEnabled = false
+        confirmPasswordTextField.delegate = self
+        self.registerButton.alpha = 0.5
+        //passwordTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
+
+
+extension SignUpViewController: UITextFieldDelegate{
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        
+        if self.verifyPassword(){
+            self.registerButton.isEnabled = true
+            self.registerButton.alpha = 1
+        }
+        else{
+            self.confirmPasswordTextField.shake()
+            self.registerButton.alpha = 0.5
+            self.registerButton.isEnabled = false
+        }
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.confirmPasswordTextField.placeholder = nil
+        self.passwordTextField.placeholder = nil
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
